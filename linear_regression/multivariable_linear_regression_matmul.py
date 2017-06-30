5f966eaf1544a077c0b90841bed33f9e984ad04f
root <root@node4.xiilab.com>
2017-06-30 16:00:00 +0900

    asdf

---
+++ linear_regression/multivariable_linear_regression_matmul.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+x_data = [[1., 0., 3., 0., 5.,], [0., 2., 0., 4., 0.]]
+y_data = [1,2,3,4,5]
+
+w = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
+b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+hypothesis = tf.matmul(w, x_data) + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+learning_rate = tf.Variable(0.08)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+train = optimizer.minimize(cost)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(2001):
+	sess.run(train)
+	print (step, sess.run(cost), sess.run(w), sess.run(b))
+
+sess.close()
